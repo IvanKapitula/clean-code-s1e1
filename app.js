e00404ac2cc80371f6e0cfb827ac49cff0746aa7
IvanKapitula <ivan.kapitula@yandex.ru>
2024-01-04 23:19:36 +0300

    refactor(extended-1.2. Alternative for multimedia): for multimedia in HTML, set the alt attribute according to this rule.

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="delete-target";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.alt='remove target button';
   deleteButton.appendChild(deleteButtonImg);
 
 
